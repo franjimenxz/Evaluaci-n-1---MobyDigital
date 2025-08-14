d794289fb44c7008a3bce79951478d1293b1450a
franco jimenez <francojimenez31@gmail.com>
2025-08-14 18:40:18 -0300

    Create class Paciente

---
+++ src/main/java/com/moby/evaluacion/model/Paciente.java
@@ -0,0 +1,72 @@
+package com.moby.evaluacion.model;
+
+public class Paciente {
+    
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String email;
+    
+    public Paciente() {
+    }
+    
+    public Paciente(Long id, String nombre, String apellido, String dni, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.email = email;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getApellido() {
+        return apellido;
+    }
+    
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    public String getDni() {
+        return dni;
+    }
+    
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    @Override
+    public String toString() {
+        return "Paciente{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", dni='" + dni + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
